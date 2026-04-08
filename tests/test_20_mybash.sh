c3e38b13090f045a0c8eb03fb15c9d6d109546a1
nusakom <2031263745@qq.com>
2026-04-09 04:43:31 +0800

    fix 20_mybash: complete implementation from reference

---
+++ tests/test_20_mybash.sh
@@ -0,0 +1,8 @@
+myfile ../exercises/20_mybash/bin/mybash
+myfile ../exercises/20_mybash/obj/mybash/main.o
+
+mysed s/unix/linux/ "unix is opensource. unix is free os."
+
+mytrans ../exercises/20_mybash/src/mytrans/text.txt
+
+mywc ../exercises/20_mybash/src/mytrans/text.txt
